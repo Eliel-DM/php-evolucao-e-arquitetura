322cf1bd16bc1b0f383acf1444236c84d2147bda
Eliel-DM <elieldiasmatos@gmail.com>
2026-08-07 14:18:12 -0300

    Finalizando o segundo exercício

--- lista-de-exercicios.php
+++ lista-de-exercicios.php
@@ -10,32 +10,40 @@ Praticar é muito importante! Por isso, preparamos uma lista de exercícios para
 */
 
 //Exercício 01
-
-
-function operacaoMatematicaPersonalizada(string $operacao, float $primeiroNumero, float $segundoNumero) {
+// Essa aqui fiz com o echo sendo enviado diretamente pela função.
+function operacaoMatematicaPersonalizada(string $operacao, float $primeiroNumero, float $segundoNumero): void {
     $resultadoFinal = 0;
     $operacaoLowCase = strtolower($operacao);
     switch ($operacaoLowCase) {
         case $operacaoLowCase == "soma":
             $resultadoFinal = $primeiroNumero + $segundoNumero;
-            echo "O resultado final da $operacao é:" . $resultadoFinal;
+            echo "O resultado final da $operacao é:" . $resultadoFinal . "\n";
             break;
         case $operacaoLowCase == "subtracao":
             $resultadoFinal = $primeiroNumero - $segundoNumero;
-            echo "O resultado final da $operacao é:" . $resultadoFinal;
+            echo "O resultado final da $operacao é:" . $resultadoFinal . "\n";
             break;
         case $operacaoLowCase == "multiplicacao":
             $resultadoFinal = $primeiroNumero * $segundoNumero;
-            echo "O resultado final da $operacao é:" . $resultadoFinal;
+            echo "O resultado final da $operacao é:" . $resultadoFinal . "\n";
             break;
         case $operacaoLowCase == "divisao":
             $resultadoFinal = $primeiroNumero / $segundoNumero;
-            echo "O resultado final da $operacao é:" . $resultadoFinal;
+            echo "O resultado final da $operacao é:" . $resultadoFinal . "\n";
             break;
         default:
-            echo "Operação inserida não é válida, favor validar se não tem acentuações ou outros caracteres. Ex de operação: 'subtracao'!";
+            echo "Operação inserida não é válida, favor validar se não tem acentuações ou outros caracteres. Ex de operação: 'subtracao'! \n";
     }
 }
-
-
 operacaoMatematicaPersonalizada("multiplicacao", 100, 10);
+
+// Exercício 02
+// Essa aqui vou fazer com o echo sendo enviado de fora da função
+
+function calcularIMC(float $peso, float $altura): float {
+    $imc = $peso / ($altura * $altura);
+    return $imc;
+}
+
+$imcCalculado = calcularIMC(85, 1.78);
+echo "O IMC calculado é: " . $imcCalculado . "\n";
