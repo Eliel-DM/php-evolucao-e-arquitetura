d4a3957525ad3c5769ce27cdc0837bf3e810b102
Eliel-DM <elieldiasmatos@gmail.com>
2026-08-07 22:16:12 -0300

    Clonei o arquivo do professor afim de deixar mais prático o entendimento dos exemplos apresentados

--- screen-match.php
+++ screen-match.php
@@ -1,6 +1,6 @@
 <?php
 
-function exibirMensagemAnoDeLancamento(int $ano): void {
+function exibeMensagemLancamento(int $ano): void {
     if ($ano > 2022) {
         echo "Esse filme é um lançamento\n";
     } elseif ($ano > 2020 && $ano <= 2022) {
@@ -10,41 +10,53 @@ function exibirMensagemAnoDeLancamento(int $ano): void {
     }
 }
 
-function inclusoNoPlano(bool $planoPrime, int $anoLancamento) {
+function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
     return $planoPrime || $anoLancamento < 2020;
 }
 
-
-echo "Bem-vindo(a) ao screen match! \n";
+echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
+
 $anoLancamento = 2022;
-$notaFilme = 8.8;
-$inclusoNoPlano = true;
+
 $quantidadeDeNotas = $argc - 1;
 $notas = [];
-$somaDeNotas = 0.0;
 
 for ($contador = 1; $contador < $argc; $contador++) {
-    $somaDeNotas += $argv[$contador];
+    $notas[] = (float) $argv[$contador];
 }
 
-$notaFilme =  $somaDeNotas / $quantidadeDeNotas;
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
 $planoPrime = true;
 
-$incluidoNoPlano = inclusoNoPlano($planoPrime, $anoLancamento);
+$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
 
 echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: " . $notaFilme . "\n";
-echo "Ano de Lançamento: " . $anoLancamento . "\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
 
-exibirMensagemAnoDeLancamento($anoLancamento);
+exibeMensagemLancamento($anoLancamento);
 
 $genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação\n",
-    "Thor: Ragnarok" => "super-Herói\n",
-    "Se beber não case" => "comédia\n",
-    default => "Generô não identificado \n",
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+    default => "gênero desconhecido",
 };
 
-echo "O gênero do filme é: " . $genero;
+echo "O gênero do filme é: $genero\n";
+
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
+
+
+echo $filme["ano"];
+
+var_dump($notas);
+sort($notas);
+var_dump($notas);
